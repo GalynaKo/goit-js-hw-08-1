4851fd75165798f82a5b2543aef2debd3c7fa73f
Galyna K <alinaamodeo@gmail.com>
2022-11-17 00:21:01 +0100

    lodash.throttle

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,11 +9,16 @@ const createCards = galleryItems.map(imageEl).join('');
 //galleryEl.addEventListener("click", onClickCard);
 function imageEl({ preview, original, description }) {
   return `
-  <li>
-      <a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-     </a>
-   </li>
+  <div class="gallery__item">
+       <a class="gallery__link" href="${original}">
+         <img
+             class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+            />
+      </a>
+ </div>
   `;
 }
 galleryEl.innerHTML = ('afterbegin', createCards);
@@ -23,7 +28,7 @@ console.log(galleryEl);
 //Посмотри в документации секцию «Options» и добавь отображение подписей к
 //изображениям из атрибута alt.Пусть подпись будет снизу и появляется
 //через 250 миллисекунд после открытия изображения.
-const moove = new SimpleLightbox('.gallery a', {
+const moove = new SimpleLightbox('gallery a', {
   captions: true,
   captionsData: 'alt',
   captionsDelay: 250,
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const vidPlayer = new Player(iframe);
