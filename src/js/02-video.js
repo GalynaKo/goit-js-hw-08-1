f989ed082fda73d6929f61a5af8c2d027339722b
Galyna K <alinaamodeo@gmail.com>
2022-11-16 23:37:21 +0100

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const vidPlayer = new Player(iframe);
+vidPlayer.on(('timeupdate', throttle(onTimeUpdate, 1000)));
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
