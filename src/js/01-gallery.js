43db4cf1b22447363444191ebb81680ba00a4177
Galyna K <alinaamodeo@gmail.com>
2022-11-16 22:30:04 +0100

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,70 +3,30 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
-
+//const SimpleLightbox = require('simplelightbox');
 console.log(galleryItems);
-const galleryEl = document.querySelector('div');
+const galleryEl = document.querySelector('.gallery');
 
-//Реализация делегирования на div.gallery и получение url большого изображения.
-galleryEl.addEventListener('click', onClickCard);
-
-//Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-
-const createCards = galleryItems.map(cardEl).join('');
-function cardEl({ preview, original, description }) {
+const createCards = galleryItems.map(imageEl).join('');
+//galleryEl.addEventListener("click", onClickCard);
+function imageEl({ preview, original, description }) {
   return `
-    <div class="gallery__item">
-       <a class="gallery__link" href="${original}">
-         <img
-             class="gallery__image"
-             src="${preview}"
-             data-source="${original}"
-             alt="${description}"
-            />
-      </a>
- </div>`;
+  <li>
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+     </a>
+   </li>
+  `;
 }
-galleryEl.insertAdjacentHTML('afterbegin', createCards);
-//console.log(createCards);
-//console.log(galleryEl);
-console.log(galleryItems);
+galleryEl.innerHTML = ('afterbegin', createCards);
+//galleryEl.addEventListener(createCards);
 
-function onClickCard(e) {
-  e.preventDefault();
-  const tach = e.target.nodeName;
-  //if filtro della meta del tach click
-  if (tach !== 'IMG') {
-    return;
-  }
-  //console.log(e.target.dataset.img);
-
-  //Подключение скрипта и стилей библиотеки модального окна basicLightbox.
-  const instance = basicLightbox.create(
-    `
-          <div class="modal">
-             <img
-                 class="modal__image"
-                  src="${e.target.dataset.source}"
-                />
-          </div> `,
-    {
-      //Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-      onShow: instance => {
-        window.addEventListener('keydown', onEscPress);
-        instance.element().querySelector('img').onclick = instance.close;
-      },
-      onClose: instance => {
-        window.removeEventListener('keydown', onEscPress);
-      },
-    }
-  );
-  //Добавь закрытие модального окна по нажатию клавиши Escape.
-  //Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно.У библиотеки basicLightbox есть метод для программного закрытия модального окна.//
-  function onEscPress(e) {
-    if (e.code === 'Escape') {
-      instance.close();
-    }
-  }
-
-  instance.show();
-}
+console.log(galleryEl);
+//Посмотри в документации секцию «Options» и добавь отображение подписей к
+//изображениям из атрибута alt.Пусть подпись будет снизу и появляется
+//через 250 миллисекунд после открытия изображения.
+const moove = new SimpleLightbox('ul.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
