ee3d798ad6f9ebc13a9209ae89a2f8bc425206ac
Galyna K <alinaamodeo@gmail.com>
2022-11-21 19:40:32 +0100

    2 try

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,9 +2,10 @@ import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
-const vidPlayer = new Player(iframe);
+const player = new Player(iframe);
 
 const VIDEOPLAY_TIME_KEY = 'videoplayer-current-time';
+
 const currentTime = localStorage.getItem(VIDEOPLAY_TIME_KEY)
   ? localStorage.getItem(VIDEOPLAY_TIME_KEY)
   : 0;
@@ -12,16 +13,19 @@ console.log();
 //Добавь в проект библиотеку lodash.throttle и сделай так,
 // чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
 
-vidPlayer.on(('timeupdate', throttle(onTimeUpdate, 1000)));
-vidPlayer.setCurrentTime(currentTime);
-vidPlayer.on('play', function () {
+player.on(('timeupdate', throttle(onTimeUpdate, 1000)));
+player.setCurrentTime(currentTime);
+
+player.on('play', function () {
   console.log('playd the video!');
 });
-vidPlayer.getVideoTitle().then(function (title) {
+
+player.getVideoTitle().then(function (title) {
   console.log('title', title);
 });
 //При перезагрузке страницы воспользуйся методом setCurrentTime()
 //для того чтобы возобновить воспроизведение с сохраненной позиции.
 function onTimeUpdate(e) {
   localStorage.setItem(VIDEOPLAY_TIME_KEY, e.seconds);
+  console.log(e.seconds);
 }
